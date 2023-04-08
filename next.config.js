0d3ab9ef903d21ebb84b58d8e31e52423bd081b6
danilichev <v.danilichev@gmail.com>
2023-04-08 16:26:04 +0300

    Prepate for github pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? "/landing/" : "",
   images: { unoptimized: true },
   output: "export",
   reactStrictMode: true,
