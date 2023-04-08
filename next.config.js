745fb91233d87531f7f08a8b733f65c87fc65348
danilichev <v.danilichev@gmail.com>
2023-04-08 17:22:32 +0300

    Add base path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? "/landing/" : "",
+  assetPrefix: isProd ? "/landing/" : undefined,
+  basePath: isProd ? "/landing" : undefined,
   images: { unoptimized: true },
   output: "export",
   reactStrictMode: true,
