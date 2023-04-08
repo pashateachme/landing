c54befb178bed5607dcbc9b358498737e98d3b45
danilichev <v.danilichev@gmail.com>
2023-04-08 18:06:05 +0300

    Update config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,25 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? "/landing/" : "",
-  basePath: isProd ? "/landing" : undefined,
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
   output: "export",
   reactStrictMode: true,
+  ...(isProd
+    ? {
+        assetPrefix: "/landing/",
+        basePath: "/landing",
+        redirects: async () => [
+          {
+            basePath: false,
+            destination: "landing/",
+            permanent: false,
+            source: "/",
+          },
+        ],
+      }
+    : {}),
 };
 
 module.exports = nextConfig;
